f621a82e2c36d87060c11785078ec0652d430d07
ORB-Union <take5079@gmail.com>
2017-04-23 21:04:34 +0900

    17년 4월 19일 유니티 수업
    Ui 수업

---
+++ 170419 유니티/Assets/ButtonInteraction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void PressdButton()
+    {
+        Debug.Log("Hello World");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 170419 유니티/Assets/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void PressdButton()
+    {
+        SceneManager.LoadScene("0419(2)");
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
